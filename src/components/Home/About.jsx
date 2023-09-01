b6a9a14d649ce23dbb4ef08c1445f7431342ca02
maxcore1120 <maxcore1120@gmail.com>
2023-09-01 16:31:53 -0700

    initial commit

---
+++ src/components/Home/About.jsx
@@ -0,0 +1,130 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import LaptopImg from "../../assets/home-main.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+
+const About = () => {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="yellow"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              Hi, my name is <span className="yellow">Pavel Lonescu </span>
+              and I'm from <span className="yellow"> Oradea, Romania.</span>
+              <br />
+              <br />
+              I graduated with a Bachelor's degree in Computer Science and Engineering in 2016.
+              <br />
+              <br />
+              As a
+              <b className="yellow"> Blockchain & Fullstack </b>developer,
+              I enjoy tackling new challenges and continuously expanding my skillset.
+              <br />
+              <br />I am expertize in
+              <b className="yellow"> Typescript, Vuejs, React/Next.js </b>
+              as well as have knowledge in programming languages such as C, Java, Python,
+              <b className="yellow"> GraphQL, and Solidity.</b>
+              <br />
+              <br />
+              I have a passion for working
+              with <b className="yellow">Smart Contract, NFT market placing, Web3, Ethereum</b> and
+              <i>
+                <b className="yellow">
+                  {" "}
+                  modern Javascript libraries and frameworks
+                </b>
+              </i>
+              &nbsp; like
+              <i>
+                <b className="yellow"> React.js / Next.js, Typescript, Vuejs</b>
+              </i>
+              <br />
+              <br />
+              I am also interested in building new
+              <i>
+                <b className="yellow"> Web Technologies and Products, </b>
+                as well as exploring areas related to
+                <b className="yellow"> Artificial Intelligence.</b>
+              </i>
+              <br />
+
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={LaptopImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Please don't hesitate to reach out to me and <span className="yellow">connect.</span>
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/maxcore1120"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  aria-label="github"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/19sajib"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  aria-label="twitter"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/19sajib/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  aria-label="linkedin"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/19sajib"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                  aria-label="instagram"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+
+}
+
+export default About
